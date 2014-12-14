2c958018476006ad8695d749d609094f2983045a
lkorth <luke@lukekorth.com>
2014-12-14 12:36:55 -0800

    Add browser based oauth

--- README.md
+++ README.md
@@ -24,7 +24,35 @@ Installation
 
 Usage
 -----
-Here is an [login activity](https://gist.github.com/3625085) sample.
+### User name and password
+
+[Login Activity](https://gist.github.com/3625085) example.
+
+### Browser based OAuth
+
+Start `FiveHundredPxOAuthActivity` with your consumer key and consumer secret
+
+```java
+Intent intent = new Intent(this, FiveHundredPxOAuthActivity.class)
+        .putExtra(FiveHundredPxOAuthActivity.CONSUMER_KEY, CONSUMER_KEY)
+        .putExtra(FiveHundredPxOAuthActivity.CONSUMER_SECRET, CONSUMER_SECRET);
+startActivityForResult(intent, FIVE_HUNDRED_PX_REQUEST_CODE);
+```
+
+The `AccessToken` will be returned to your `Activity` in `Activity#onActivityResult`
+after the user authorizes your app
+
+```java
+@Override
+protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (requestCode == FIVE_HUNDRED_PX_REQUEST_CODE) {
+        if (resultCode == Activity.RESULT_OK) {
+            AccessToken accessToken = data.getParcelableExtra(FiveHundredPxOAuthActivity.ACCESS_TOKEN);
+        }
+    }
+}
+```
 
 Examples
 --------
---
+++ src/main/java/com/fivehundredpx/api/auth/FiveHundredPxOAuthHelper.java
@@ -0,0 +1,59 @@
+package com.fivehundredpx.api.auth;
+
+import android.net.Uri;
+import android.text.TextUtils;
+
+import com.fivehundredpx.api.BuildConfig;
+import com.fivehundredpx.api.auth.AccessToken;
+import com.fivehundredpx.api.auth.OAuthConstants;
+
+import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
+import oauth.signpost.commonshttp.CommonsHttpOAuthProvider;
+import oauth.signpost.exception.OAuthCommunicationException;
+import oauth.signpost.exception.OAuthExpectationFailedException;
+import oauth.signpost.exception.OAuthMessageSignerException;
+import oauth.signpost.exception.OAuthNotAuthorizedException;
+
+public class FiveHundredPxOAuthHelper {
+
+    public static final String FIVE_HUNDRED_PX_CALLBACK_URL_SCHEME = "fivehundredpx";
+
+    private static final String FIVE_HUNDRED_PX_API_URL = "https://api.500px.com/v1";
+    private static final String REQUEST_TOKEN_URL = FIVE_HUNDRED_PX_API_URL + "/oauth/request_token";
+    private static final String ACCESS_TOKEN_URL = FIVE_HUNDRED_PX_API_URL + "/oauth/access_token";
+    private static final String AUTHORIZE_URL = FIVE_HUNDRED_PX_API_URL + "/oauth/authorize";
+
+    private CommonsHttpOAuthConsumer mOAuthConsumer;
+    private CommonsHttpOAuthProvider mOAuthProvider;
+
+    private String mVerifier;
+
+    public FiveHundredPxOAuthHelper(String consumerKey, String consumerSecret) {
+        if (TextUtils.isEmpty(consumerKey) || TextUtils.isEmpty(consumerSecret)) {
+            throw new IllegalArgumentException(getClass().getSimpleName() + " must be started with" +
+                    "a non-empty consumerKey and consumerSecret");
+        }
+
+        mOAuthConsumer = new CommonsHttpOAuthConsumer(consumerKey, consumerSecret);
+        mOAuthProvider = new CommonsHttpOAuthProvider(REQUEST_TOKEN_URL,
+                ACCESS_TOKEN_URL, AUTHORIZE_URL);
+    }
+
+    public String getAuthorizationUrl() throws OAuthCommunicationException, OAuthExpectationFailedException,
+            OAuthNotAuthorizedException, OAuthMessageSignerException {
+        return mOAuthProvider.retrieveRequestToken(mOAuthConsumer, FIVE_HUNDRED_PX_CALLBACK_URL_SCHEME +
+                "://" + BuildConfig.FIVE_HUNDRED_PX_OAUTH_CALLBACK_URL + "/");
+    }
+
+    public void parseAuthorizationResponse(Uri redirectedUri) {
+        mVerifier = redirectedUri.getQueryParameter(OAuthConstants.VERIFIER);
+    }
+
+    public AccessToken getAccessToken() throws OAuthCommunicationException, OAuthExpectationFailedException,
+            OAuthNotAuthorizedException, OAuthMessageSignerException {
+        mOAuthProvider.retrieveAccessToken(mOAuthConsumer, mVerifier);
+
+        return new AccessToken(mOAuthConsumer.getToken(), mOAuthConsumer.getTokenSecret());
+    }
+
+}
